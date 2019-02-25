d4a90f66d4244c57ed1181ad331dccc447e1264a
Laura Unaeze <l.unaeze5@gmail.com>
2019-02-25 15:19:28 -0700

    feat: Added a middleware in server/app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const graphqlHTTP = require("express-graphql");
 
 const app = express();
+
+// middleware
+app.use("/graphql", graphqlHTTP({
+  // leaving empty for now
+}));
 
 app.listen(4000, () => {
   console.log("Welcome to Port 4000. Now listening for request...");
