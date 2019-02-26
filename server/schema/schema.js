bb116424b763cc30f01335c4ac929a68bc2f1390
Laura Unaeze <l.unaeze5@gmail.com>
2019-02-26 10:13:03 -0700

    feat: Added lodash to server. Added dummy data and resolver function.

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,5 @@
 const graphql = require("graphql");
+const _= require("lodash");
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
@@ -8,6 +9,14 @@ const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
   2. Define relationsips between types.
   3. Define root queries
 */
+
+// dummy data
+const books = [
+  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+];
+
 
 // 1. Define a type
 const BookType = new GraphQLObjectType({
@@ -32,6 +41,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parent, args){
         // code to get data from db or other source
+        return _find(books, {id: args.id});
       }
     }
   }
