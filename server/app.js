c1d1e7489b8016d8e9d9561b29277a7bcad384ae
Laura Unaeze <l.unaeze5@gmail.com>
2019-02-27 12:17:33 -0700

    feat: Added connection to mlab database.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const password = require("./topsecret");
+const mongoose = require("mongoose");
+
 
 const app = express();
+
+// connect to mlab databaase
+mongoose.connect(`mongodb+srv://laura:${password}@graphql-tutorial-from-netninja-aglto.mongodb.net/test?retryWrites=true`);
+mongoose.connection.once("open", () => {
+  console.log("Connected to database!");
+})
 
 // middleware
 app.use("/graphql", graphqlHTTP({
