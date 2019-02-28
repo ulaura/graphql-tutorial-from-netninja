75a7f3effbc5853f863732be4886d0a09622ab6f
Laura Unaeze <l.unaeze5@gmail.com>
2019-02-28 11:18:08 -0700

    feat: Added cors to project. Bound gql query results to BookList component

--- server/app.js
+++ server/app.js
@@ -3,9 +3,13 @@ const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const password = require("./topsecret");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 
 const app = express();
+
+// allow cross-origin requests
+app.use(cors());
 
 // connect to mlab databaase
 mongoose.connect(`mongodb+srv://laura:${password}@graphql-tutorial-from-netninja-aglto.mongodb.net/test?retryWrites=true`);
